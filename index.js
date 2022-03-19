b2f42e61d9a2272b08e045d14784525639764303
prantobiswas321 <rahulbd321@gmail.com>
2022-03-20 01:25:32 +0600

    env edited

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
